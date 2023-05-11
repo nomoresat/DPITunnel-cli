6d535f60f2f15ddcc4add36a87ffb8bb203a6f48
Prateek Ganguli <prateek.ganguli@gmail.com>
2023-05-11 14:05:29 +0530

    openssl: Removed calls to deprecated ERR_load_*() OpenSSL now loads error strings automatically so these functions are not needed. https://www.openssl.org/docs/manmaster/man7/migration_guide.html

--- autoconf.cpp
+++ autoconf.cpp
@@ -483,8 +483,6 @@ int run_autoconf() {
         SSL_library_init();
         OpenSSL_add_all_algorithms();
         SSL_load_error_strings();
-        ERR_load_BIO_strings();
-        ERR_load_crypto_strings();
 
         store = gen_x509_store();
         if (store == NULL) {
